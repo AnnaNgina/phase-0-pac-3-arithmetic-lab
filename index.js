f47c2eabc82839af91b78fe6831b7d14c2385075
annastacia <annastaciangina96@gmail.com>
2022-06-06 00:30:46 +0300

    commit tree 5db9b1775cad9a7979581b02d2ac6de8908fa1a7

---
+++ index.js
@@ -0,0 +1,28 @@
+//let  a= 11;
+//let b =80;
+//let c= a+b;
+function add (a, b)
+
+{
+
+    return a+b;
+  
+  // console.log('The sum of ' + a + ' and ' + b + ' is: ' + c);
+   
+}
+
+    function subtract(a, b){
+ return a-b;
+    }
+
+function multiply(a ,b){
+    return a *b;
+}
+ function divide(a , b){
+ return a/b;
+ }
+ let n =10;
+ n++;
+ function n () {
+ return 'n'
+ }
